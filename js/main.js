a87f4479107154e4636faa8d59535ea9a919009e
jondauz <jondauz@gmail.com>
2014-07-30 19:31:43 -0700

    Adding comments

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var connectFour = (function(){
 		playersArray,
 		currentPlayerIndex;
 
+	// Initialize the game.
 	function initGame(){
 	
 		$('#restartBtn').unbind('click')
@@ -21,6 +22,7 @@ var connectFour = (function(){
 
 	}	
 
+	// Add listeners for gameboard.
 	function addListeners(){
 
 		$('.gameBoard li').click(function(){
@@ -39,18 +41,21 @@ var connectFour = (function(){
 
 	}
 
+	// Remove listeners when game is done.
 	function removeListeners(){ 
 		$('.gameBoard li').unbind('click');
 	}
 	
+	// makeChoice() function marks the game board depending on where the player clicks on the gameboard.
 	function makeChoice(el){
 
 		var clickedId = el.attr('id'),
 			column = clickedId.charAt(0);
 		
+		// Go through gameboard and place tile where it needs to.
 		for(var i=1;i<=7;i++) {
-			console.log(i);
 			var currentSection = $('.gameBoard').find('#'+column+i);
+			// If spot in game board is taken then move up the column.
 			if(!currentSection.hasClass('selected')) {
 				currentSection.addClass('selected');
 				if(currentPlayerIndex == 0) {
@@ -58,13 +63,15 @@ var connectFour = (function(){
 				} else {
 					currentSection.addClass('red');
 				}
-				playersArray[currentPlayerIndex].playerChoices.push([column,i]);	
+				// Add the player choices array an array with coordinates of player's move.	
+				playersArray[currentPlayerIndex].playerChoices.push([column,i]); // [0] = column & [1] = row	
 				return;
 			}
 		}
 
 	}
 
+	// Change the current player to the next player.
 	function nextPlayer(){
 	
 		if(currentPlayerIndex == 0) {
@@ -73,11 +80,13 @@ var connectFour = (function(){
 			currentPlayerIndex--;
 		}
 
+		// Change feedback to depict current player.
 		$('#gameFeedback').text(playersArray[currentPlayerIndex].playerName + "'s turn!");
 		changeTextColor();
 	
 	}
 
+	// Check for vertical winning sequence.
 	function checkColumns(){
 		
 		var currentPlayer = playersArray[currentPlayerIndex],
@@ -86,33 +95,34 @@ var connectFour = (function(){
 			prevRow,
 			winningSet = [];
 			
+		// Sort the choices that player makes by the first item in the choice array. 
 		var sortedArray = currentPlayer.playerChoices.sort(function (a,b) {
 		    if (a[0] < b[0]) return -1;
 		    if (a[0] > b[0]) return 1;
 		    return 0;
 		});
 
-		//console.log(sortedArray.toString());
-
+		// Go through each item in the array and find if there is a winning sequence.
+		// Look for if rows go in sequence. Example: 1, 2, 3, 4 but all have the same column. Example: A
 		for(var i=0;i<sortedArray.length;i++) {
-			//debugger;
+			// If no longer looking in same column, reset counters and variables. 
 			if(currentColumn!=sortedArray[i][0]) {
 				currentColumn = sortedArray[i][0];
 				sequenceCounter=0;
 				prevRow="";
-				winningSet=[];
 			}
 			if(prevRow!="") {
 				var neededRow = prevRow + 1,
 					actualRow = sortedArray[i][1];
+				// Check to see if row goes in a sequence. If current row is same as previous row plus 1.
 				if(neededRow == actualRow) {
-					winningSet.push(sortedArray[i]);
 					sequenceCounter++;
 				} 
 			} 
+			// Change for next item in array.
 			prevRow = sortedArray[i][1];		
 			if(sequenceCounter == 3) {
-				console.log(winningSet.toString());
+				// If sequence is 3 then winning sequence found. 
 				console.log('checkColumns()');
 				return true;
 			}
@@ -130,28 +140,31 @@ var connectFour = (function(){
 			prevColum3,
 			winningSet = [];
 
+		//  Sort array by row. 
 		var sortedArray = currentPlayer.playerChoices.sort(function (a,b) {
 		    if (a[1] < b[1]) return -1;
 		    if (a[1] > b[1]) return 1;
 		    return 0;
 		});
 
+		// Go through each item in the array and find if there is a winning sequence.
+		// Winning sequence is if columns go in sequence, Example: A,B,C,D but have the same row such as row 1. 
 		for(var i=0;i<sortedArray.length;i++) {
+			// If no longer looking in same row, reset counters and variables. 
 			if(currentRow!=sortedArray[i][1]) {
 				currentRow = sortedArray[i][1];
 				sequenceCounter=0;
 				prevColumn="";
-				winningSet=[];
 			}
 			if(prevColumn!="") {
+				// Check to see if column goes in a sequence. If current column is same as previous row plus 1.
 				if(String.fromCharCode(prevColumn.charCodeAt(0) + 1).toLowerCase() == sortedArray[i][0].toLowerCase()) {
-					winningSet.push(sortedArray[i]);
 					sequenceCounter++;
 				} 
 			}
 			prevColumn = sortedArray[i][0];
 			if(sequenceCounter == 3) {
-				console.log(winningSet.toString());
+				// If sequence is 3 then winning sequence found. 
 				console.log('checkRows()');
 				return true;
 			}
@@ -166,44 +179,44 @@ var connectFour = (function(){
 		var currentPlayer = playersArray[currentPlayerIndex],
 			sequenceCounter = 0;
 		
+		// Sort array by column. 
 		var sortedArray = currentPlayer.playerChoices.sort(function (a,b) {
 		    if (a[0] < b[0]) return -1;
 		    if (a[0] > b[0]) return 1;
 		    return 0;
 		});
-
-		if(currentPlayer.numberOfMoves >= 4) {
 			
-			for(var i=0;i<sortedArray.length;i++) {
-			
-				sequenceCounter = 0;
-			
-				var currentColumn = sortedArray[i][0],
-					currentRow = sortedArray[i][1];
-			
-				for(var j=0;j<sortedArray.length;j++){
-						
-					var checkColumn = sortedArray[j][0];
-					var checkRow = sortedArray[j][1];
-
-					if((String.fromCharCode(currentColumn.charCodeAt(0) + 1) == checkColumn) && ((currentRow + 1) == checkRow)) {
-						sequenceCounter++;
-						currentColumn = checkColumn,
-						currentRow = checkRow;
-						console.log(sequenceCounter);
-						if(sequenceCounter == 3) {
-							console.log('checkAscDiagonals()');
-							return true;
-						}
+		for(var i=0;i<sortedArray.length;i++) {
+		
+			// Reset counter if winning sequence not found for current column.
+			sequenceCounter = 0;
+		
+			var currentColumn = sortedArray[i][0],
+				currentRow = sortedArray[i][1];
+		
+			for(var j=0;j<sortedArray.length;j++){
 					
+				var checkColumn = sortedArray[j][0];
+				var checkRow = sortedArray[j][1];
+
+				if((String.fromCharCode(currentColumn.charCodeAt(0) + 1) == checkColumn) && ((currentRow + 1) == checkRow)) {
+					sequenceCounter++;
+					// If sequence found then change current for next sequence
+					currentColumn = checkColumn,
+					currentRow = checkRow;
+					if(sequenceCounter == 3) {
+						console.log('checkAscDiagonals()');
+						// Return true if a sequence of 3 found. Means that there are 4 in a row.
+						return true;
 					}
-					
-			  	} 
+				
+				}
+				
+		  	} 
 
-	  		}
+  		}
 
-		}
-
+  		// Return false if no sequence is found. 
 		return false;
 
 	}
@@ -213,6 +226,7 @@ var connectFour = (function(){
 		var currentPlayer = playersArray[currentPlayerIndex],
 			sequenceCounter = 0;
 		
+		// Sort array first by the column then by row.
 		var sortedArray = currentPlayer.playerChoices.sort(function (a,b) {
 		    if (a[0] < b[0]) return -1;
 		    if (a[0] > b[0]) return 1;
@@ -221,42 +235,49 @@ var connectFour = (function(){
 		    return 0;
 		});
 		
-		if(currentPlayer.numberOfMoves >= 4) {
-			for(var i=0;i<sortedArray.length;i++) {
-				sequenceCounter = 0;
-				var currentColumn = sortedArray[i][0];
-				var currentRow = sortedArray[i][1];
-				for(var j=0;j<sortedArray.length;j++){
-						
-					var checkColumn = sortedArray[j][0];
-					var checkRow = sortedArray[j][1];
-
-					if((String.fromCharCode(currentColumn.charCodeAt(0) + 1) == checkColumn) && ((currentRow - 1) == checkRow)) {
-						sequenceCounter++;
-						currentColumn = checkColumn,
-						currentRow = checkRow;
-						if(sequenceCounter == 4) {
-							console.log('checkDescDiagonals()');
-							return true;
-						}
+		for(var i=0;i<sortedArray.length;i++) {
+			// Reset counter if winning sequence not found for current column.
+			sequenceCounter = 0;
+			var currentColumn = sortedArray[i][0];
+			var currentRow = sortedArray[i][1];
+			for(var j=0;j<sortedArray.length;j++){
 					
+				var checkColumn = sortedArray[j][0];
+				var checkRow = sortedArray[j][1];
+
+				if((String.fromCharCode(currentColumn.charCodeAt(0) + 1) == checkColumn) && ((currentRow - 1) == checkRow)) {
+					sequenceCounter++;
+					// If sequence found then change current for next sequence
+					currentColumn = checkColumn,
+					currentRow = checkRow;
+					if(sequenceCounter == 3) {
+						console.log('checkDescDiagonals()');
+						// Return true if a sequence of 3 found. Means that there are 4 in a row.
+						return true;
 					}
-					
-			  	} 
+				
+				}
+				
+		  	} 
 
-	  		}
+  		}
 
-		}
-
+  		// Return false if no sequence is found. 
 		return false;
 
 	}
 
+	// Runs when someone wins the game.
 	function finishGame(){
-		console.log(playersArray[currentPlayerIndex])
+		
+		// Create feedback that player has won the game.
 		$('#gameFeedback').text(playersArray[currentPlayerIndex].playerName+ ' wins!');
 		changeTextColor();
+
+	 	// Remove listeners so that players cannot makes anymore choices.
 		removeListeners();
+
+		// If restartBtn is clicked then restart the game and clear the gameboard.
 		$('#restartBtn').show();
 		$('#restartBtn').click(function(){
 			$('.gameBoard li').removeAttr('class');
@@ -264,6 +285,7 @@ var connectFour = (function(){
 		});
 	}
 
+	// Used to change feedback color, which depicts the current player. 
 	function changeTextColor() {
 		if(currentPlayerIndex == 0) {
 			$('#gameFeedback').addClass('blue');
@@ -274,6 +296,7 @@ var connectFour = (function(){
 		}
 	}
 
+	// Person Object
 	function Person(name){
 		this.playerName = name;
 		this.playerChoices = [];
